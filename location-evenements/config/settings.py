4ae0baca1c02dd298e5f4a46adb77477019b37d5
christiano <miandrisoachristiano@gmail.com>
2026-05-24 17:14:55 +0300

    Configuration settings.py pour Render

--- location-evenements/config/settings.py
+++ location-evenements/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-55p0cg$*w*116wog5_pad2tj8hpqgm!x7ka+$)t*&5hgp^8=mk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 
 # Application definition
