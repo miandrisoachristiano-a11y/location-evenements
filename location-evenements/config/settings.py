b903890f41ca33334921651df0a611e49e3ec85d
christiano <miandrisoachristiano@gmail.com>
2026-05-28 13:13:10 +0300

    Refonte graphique de la page de connexion admin et personnalisation du theme Django Admin

--- location-evenements/config/settings.py
+++ location-evenements/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-55p0cg$*w*116wog5_pad2tj8hpqgm!x7ka+$)t*&5hgp^8=mk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
@@ -145,7 +145,7 @@ JAZZMIN_SETTINGS = {
 JAZZMIN_UI_TWEAKS = {
     "navbar": "navbar-dark",
     "theme": "lumen",
-    "sidebar": "sidebar-light-primary", 
+    "sidebar": "sidebar-dark-primary", 
     "sidebar_nav_child_indent": True,
     "sidebar_nav_compact_style": False,
     "sidebar_nav_legacy_style": False,
